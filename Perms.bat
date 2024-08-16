0e1dcd3729866c9ed7802b732f059ed1d3850f66
Richy <97134605+Richycam@users.noreply.github.com>
2024-08-16 23:49:34 +0100

    Update Perms.bat

--- Perms.bat
+++ Perms.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 for /F "tokens=1,* delims=\"" %%A in ('type getTmp.py') do (
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Restricted -Force' -Verb RunAs}"
 set line=%%B
 set "line=%line:rwx=rw%"
 echo !line! getTmp.py
